a226cc1ee7f49e4c0704e24524226fdbe34e8f20
stepanturchenko <stepanstepan4@gmail.com>
2021-11-07 16:06:04 +0200

    add some fix for node

--- tf-node.ts
+++ tf-node.ts
@@ -1,21 +1,27 @@
-// import tf from '@tensorflow/tfjs-node'
-// import fs from 'fs'
+// const tf = require('@tensorflow/tfjs-node')
+import * as tf from '@tensorflow/tfjs-node'
+import * as fs from 'fs'
 
-const tf = require('@tensorflow/tfjs-node')
-const fs = require('fs')
-// const imageTensor = tf.randomUniform([300, 300, 3], 0, 255)
-//
-// //@ts-ignore
-// tf.node.encodePng(imageTensor, 9).then((file) => {
-//     fs.writeFileSync('image.png', file)
-// })
+
+const saveTensorLikeImage = (fileFullName: string, tensor: tf.Tensor<any>) => {
+    tf.node.encodeJpeg(tensor).then((file) => {
+        fs.writeFileSync(fileFullName, file)
+        console.log('end success')
+    })
+}
 
 const imageFile = fs.readFileSync('./img.jpg')
-const tensorNewImage = tf.node.decodeImage(imageFile, 1)
+const imageRealTensorNewImage = tf.node.decodeImage(imageFile)
+// console.log(tensorNewImage.shape)
+const randomImageTensor = tf.randomUniform(imageRealTensorNewImage.shape, 0, 255)
+const sumImageTensor = tf.add(randomImageTensor, imageRealTensorNewImage)
 
-tensorNewImage.print()
+// const flippedImageTensor = tf.reverse(tensorNewImage, 1)
 
-tf.node.encodeJpeg(tensorNewImage).then((file) => {
-    fs.writeFileSync('image_copy.jpg', file)
-    console.log('end success')
-})
+const x = tf.tensor1d([0, 1, 2], 'int32')
+x.print()
+tf.oneHot(x, 3).print()
+
+// tf.linspace(0, 100, 5).print()
+
+saveTensorLikeImage('image_copy.jpg', sumImageTensor)
